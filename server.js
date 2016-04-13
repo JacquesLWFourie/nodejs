7a2c58191e49a9167619eba1d9808bc480e4dbbd
Jacques <jfourie@deloite.co.za>
2016-04-13 11:45:20 +0200

    Added the new view

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 var http = require('http');
+var url = require('url');
+
 var pages = [
-	{route:'',output:'wooHooaaaa'},
-	{route:'/about/this',output:'multi level routing'},
-	{route:'/about/node',output:'some more routing'},
-	{route:'another page',output:function(){return 'Heres '+this.route;}}
+	{id:'1',route:'',output:'wooHooaaaa'},
+	{id:'2',route:'about',output:'simple routing with node'},
+	{id:'3',route:'/about/node',output:'some more routing'},
+	{id:'4',route:'another page',output:function(){return 'Heres '+this.route;}}
 ];
 http.createServer(function (request,response){
-	var lookup = (decodeURI(request.url));
-	console.log('lookup ' + lookup);
-	
-	pages.forEach(function(page){
-		
-		if(page.route === lookup){
-			response.writeHead(200,{'Content-type':'text/html'});
-			response.end(typeof page.output === 'function' ? page.output() : page.output);
-		}
-	});
+	var id = url.parse(decodeURI(request.url),true).query.id;
+	console.log('id ' + id);
+	if(id){
+		pages.forEach(function(page){
+			if(page.id === id){
+				response.writeHead(200,{'Content-type':'text/html'});
+				response.end(typeof page.output === 'function' ? page.output() : page.output);
+			}
+		});
+
+	}
 	if(!response.finished){
 		response.writeHead(404);
 		response.end('page not found !!!');
