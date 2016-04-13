c4b50e4718a29911eced276af075c2b531a168d3
Jacques <jfourie@deloite.co.za>
2016-04-13 10:33:48 +0200

    first Commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var path = require('path');
+var pages = [
+	{route:'',output:'wooHoo'},
+	{route:'about',output:'the about page'},
+	{route:'another page',output:function(){return 'Heres '+this.route;}}
+];
+http.createServer(function (request,response){
+	var lookup = path.basename(decodeURI(request.url));
+	console.log('lookup ' + lookup);
+	
+	pages.forEach(function(page){
+		console.log(page.route);
+		if(page.route === lookup){
+			response.writeHead(200,{'Content-type':'text/html'});
+			response.end(typeof page.output === 'function' ? page.output() : page.output);
+		}
+	});
+	if(!response.finished){
+		response.writeHead(404);
+		response.end('page not found');
+	}
+	/*
+	if(!response.finished){
+		response.writeHead(404);
+		response.end('page not found');
+	}
+	*/
+}).listen(8080);
